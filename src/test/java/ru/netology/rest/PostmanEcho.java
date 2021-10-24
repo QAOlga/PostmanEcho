affa0ebf07b867157090ed2e70a2c8e230149b9b
OlgaMikhaylyukova <olele@inbox.ru>
2021-10-24 17:31:44 +0300

    test fixed

--- src/test/java/ru/netology/rest/PostmanEcho.java
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -19,7 +19,7 @@ public class PostmanEcho {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(300)
+                .statusCode(200)
                 .body("data", equalTo("postmanecho"));
     }
 
